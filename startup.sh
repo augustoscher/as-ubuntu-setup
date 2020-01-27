83b31ef3e365572ae2a813099ba96a1435765b59
Augusto <augusto.scher@resultadosdigitais.com.br>
2020-01-26 22:03:12 -0300

    Updating

--- startup.sh
+++ startup.sh
@@ -18,13 +18,13 @@ read git_config_user_email
 git config --global user.email $git_config_user_email
 clear
 
-echo "Can I set VIM as your default GIT editor for you? (y/n)"
-read git_core_editor_to_vim
-if echo "$git_core_editor_to_vim" | grep -iq "^y" ;then
-	git config --global core.editor vim
-else
-	echo "Okay, no problem. :) Let's move on!"
-fi
+# echo "Can I set VIM as your default GIT editor for you? (y/n)"
+# read git_core_editor_to_vim
+# if echo "$git_core_editor_to_vim" | grep -iq "^y" ;then
+git config --global core.editor vim
+# else
+# 	echo "Okay, no problem. :) Let's move on!"
+# fi
 
 echo "Generating a SSH Key"
 ssh-keygen -t rsa -b 4096 -C $git_config_user_email
