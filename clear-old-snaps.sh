f4cfff7a8e3940151d31f8b3203541e679fb28d8
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2020-01-31 09:30:18 -0300

    clear old snaps revisions

--- clear-old-snaps.sh
+++ clear-old-snaps.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# Removes old revisions of snaps
+
+echo "Cleaning systemd journal logs"
+sudo journalctl --vacuum-time=1d
+
+echo ""
+echo "Removing old revisions of snaps"
 # CLOSE ALL SNAPS BEFORE RUNNING THIS
 set -eu
 snap list --all | awk '/disabled/{print $1, $3}' |
